6108fb2051515785abd1186756587ae9a434f8fd
Andrey <andrii.tykhanovych.pl@gmail.com>
2020-08-22 15:46:04 +0200

    Generate deployables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component'
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+  { path: 'students', component: StudentsComponent },
+  { path: 'edit/:id', component: StudentEditComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent }
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { StudentNewComponent } from './student-new/student-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    StudentEditComponent,
+    NotFoundComponent,
+    StudentNewComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
